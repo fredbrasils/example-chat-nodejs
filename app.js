5ed1b3f7a138bdbe041f6466a7350e6af5cfb6b5
Fred Brasil <fredswimmer@gmail.com>
2017-01-04 17:45:03 -0300

    inform when somebody get in and get out of the conversation

--- app.js
+++ app.js
@@ -8,3 +8,26 @@ var porta = process.env.PORT || 3000;
 http.listen(porta,function(){
     console.log("starting server");
 });
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+  
+    // Use socket to communicate with this particular client only, sending it it's own id
+    socket.emit('welcome', {id: socket.id });
+      
+    socket.on('disconnect', function(){
+        console.log('user disconnected:' + socket.id);
+        
+        var data = [];
+        var sockets = app.get('sockets_id');
+        
+        for(index=0; index < sockets.length; index++){
+           if(sockets[index].id === socket.id){
+               data = sockets[index];
+               sockets.splice(index, 1);
+           }
+       }    
+        
+        app.get('io').emit(data.room,{user:data.user, msg:" :: saiu da conversa!"});
+    });
+});
--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,12 +1,9 @@
 module.exports = function(app){
     
+    var sockets_id = [];
+    
     app.get('/chat',function(req,res){
-//        var connection = app.infra.connectionFactory();
-//        var productDAO = new app.infra.ProductDAO(connection);
 
-//        productDAO.list(function(err, results){
-//            res.render('home/chat',{livros:results.rows});
-//        });
         var allchat = req.body;
         res.render('chat/chat',{room:req.query.room,
                                 user:req.query.user
@@ -16,9 +13,28 @@ module.exports = function(app){
     
    app.post("/",function(req,res){
        var allchat = req.body;
-       console.log(allchat);
-       app.get('io').emit(allchat.room,allchat);
-       //res.redirect('/chat');
+//       console.log(allchat);
+       var msg = ": " + allchat.msg;
+       var user = allchat.user;
+       
+       app.get('io').emit(allchat.room,{user:user, msg:msg});
+       res.redirect('/chat'); //doesn't work because I am using ajax
+    });
+    
+    app.post("/welcome",function(req,res){
+       var data = req.body;
+       
+       sockets_id.push({  id: data.id,
+                        room: data.room,
+                        user: data.user
+                       });
+        
+       app.set('sockets_id',sockets_id);
+//       console.log(app.get('sockets_id'));
+       
+       app.get('io').emit(data.room,{user:data.user, msg:" :: Acabou de entrar na conversa!"});
+        
+       res.redirect('/chat'); //doesn't work because I am using ajax
     });
     
 }
